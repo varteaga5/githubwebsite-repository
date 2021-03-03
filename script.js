c711a2731d47cb5464b1253807f29f9363b3dca7
vincent <vincent.arteaga@gmail.com>
2021-03-02 18:59:59 -0800

    updated 3.2.21

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2 = document.createElement('h2');
+h2.textContent = "This content was added by JavaScript, Vincent is a Full Stack Developer and Real Estate Investor.";
+document.querySelector('body').appendChild(h2);
